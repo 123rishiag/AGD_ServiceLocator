991da3e681c787acd59903c6221cece824c4f76d
Rishabh Garg <narishabh.garg@gmail.com>
2024-10-31 18:08:44 +0530

    Implemented Generic Singleton for all Singleton Services

--- Assets/Scripts/Player/PlayerService.cs
+++ Assets/Scripts/Player/PlayerService.cs
@@ -7,7 +7,7 @@ using ServiceLocator.Sound;
 
 namespace ServiceLocator.Player
 {
-    public class PlayerService : MonoBehaviour
+    public class PlayerService : GenericMonoSingleton<PlayerService>
     {
         [SerializeField] public PlayerScriptableObject playerScriptableObject;
 
@@ -18,22 +18,6 @@ namespace ServiceLocator.Player
         private int health;
 
         public int Money { get; private set; }
-
-        public static PlayerService Instance { get { return instance; } }
-        private static PlayerService instance;
-
-        private void Awake()
-        {
-            if(instance == null)
-            {
-                instance = this;
-            }
-            else
-            {
-                Destroy(this.gameObject);
-                Debug.LogError("Singleton of Player Service is trying to create a Second Instance.");
-            }
-        }
 
         private void Start()
         {
--- Assets/Scripts/Sound/SoundService.cs
+++ Assets/Scripts/Sound/SoundService.cs
@@ -1,29 +1,14 @@
+using ServiceLocator.Player;
 using System;
 using UnityEngine;
 
 namespace ServiceLocator.Sound
 {
-    public class SoundService : MonoBehaviour
+    public class SoundService : GenericMonoSingleton<SoundService>
     {
         [SerializeField] private SoundScriptableObject soundScriptableObject;
         [SerializeField] private AudioSource audioEffects;
         [SerializeField] private AudioSource backgroundMusic;
-
-        public static SoundService Instance { get { return instance; } }
-        private static SoundService instance;
-
-        private void Awake()
-        {
-            if(instance == null)
-            {
-                instance = this;
-            }
-            else
-            {
-                Destroy(this.gameObject);
-                Debug.LogError("Singleton of Sound Service is trying to create a Second Instance.");
-            }
-        }
 
         private void Start()
         {
